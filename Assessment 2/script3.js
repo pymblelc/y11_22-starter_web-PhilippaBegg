81e4e16ee732f9bfcbe28ac8bfc8042155d47433
Philippa <pbegg@pymblelc.nsw.edu.au>
2022-06-26 18:35:47 +1000

    ayya8

--- Assessment 2/script3.js
+++ Assessment 2/script3.js
@@ -1,106 +1,57 @@
 var faces = []; //undefined
-
 let myImage = document.getElementById("myPhoto");
 let myButton = document.getElementById("btnAnalyse");
 let results = document.getElementById("myText");
 let btnClear = document.getElementById("btnClear");
 let btnDetermine = document.getElementById("btnDetermine");
-
 let imageURL = myImage.src;
-
-// var hair;
-// var glasses;
-
-// myButton.addEventListener("click", function () {
-//   ImageAPI.analyseFaces(imageURL, function (data) {
-//     console.log(data);
-//     // for(initial condition: terminating condition: increment)
-//     // i ++ >>> i = i + 1
-//     for (let i = 0; i < data.length; i++) {
-//       hair = data[i].faceAttributes.facialHair.moustache;
-//       let hairText = "";
-//       if (hair == 1) {
-//         hairText = "moustache.";
-//       } else {
-//         hairText = "no moustache.";
-//       }
-//       glasses = data[i].faceAttributes.glasses; //[0]-only recieves data for one person.
-//       let glassesText = "";
-//       if (glasses == "NoGlasses") {
-//         glassesText = "no glasses";
-//       } else {
-//         glassesText = "glasses";
-//       }
-//       let text = "<p>" + "Face " + (i + 1) + " has " + glassesText + " and has " + hairText + "</p>";
-
-//       faces.push({
-//         hair: hair,
-//         hairText: hairText,
-//         glasses: glasses,
-//         glassesText: glassesText,
-//         finalComment: text,
-//       });
-
-//       results.innerHTML += text;
-//     }
-//   });
-// });
-
-// btnDetermine.addEventListener("click", function () {
-//   results.innerHTML = " ";
-//   console.log(faces);
-//   for (let i = 0; i < faces.length; i++) {
-//     if (faces[i].hair == 1 && faces[i].glasses != "NoGlasses") {
-//       faces[i].finalComment = "<p>" + "Face " + (i + 1) + " is allowed in." + "</p>";
-//     } else {
-//       faces[i].finalComment = '<p>' + "Face " + (i + 1) + " is not allowed in." + '<p>';
-//     }
-//     results.innerHTML += faces[i].finalComment;
-//   }
-// });
-
 var age;
-
-
-myButton.addEventListener("click", function () {
-  ImageAPI.analyseFaces(imageURL, function (data) {
-    console.log(data);
-    // for(initial condition: terminating condition: increment)
-    // i ++ >>> i = i + 1
-    for (let i = 0; i < data.length; i++) {
-      age = data[i].faceAttributes.age;
-      let ageText = "";
-      if (age > 49) {
-        ageText = "over 50.";
-      } else {
-        ageText = "not over 50.";
+let findAgesCalled = false;
+function findAges(){
+  // to ensure that this function is only called once
+  if (findAgesCalled == false) {
+    findAgesCalled = true;
+    ImageAPI.analyseFaces(imageURL, function (data) {
+      // Loop through this for every face identified in the image
+      for (let i = 0; i < data.length; i++) {
+        age = data[i].faceAttributes.age;
+        // Adds this face to the array and stores its age
+        faces.push({
+          age
+        });
       }
-      let text = "<p>" + "Face " + (i + 1) + " is " + ageText + "</p>";
-
-      faces.push({
-        age: age,
-        ageText: ageText,
-        finalComment: text,
-      });
-
+    });
+  } 
+}
+// As soon as this page is loaded, find the ages for all the faces
+$(document).ready(function () {
+  findAges();
+});
+myButton.addEventListener("click", function () {
+    // Loop through every face stored in the faces[] array
+    for (let i = 0; i < faces.length; i++) {
+      age = faces[i].age;
+      // Print the appropriate message for each face
+      let text = "";
+      if (faces[i].age > 40) {
+        text = "<p>" + "Face " + (i + 1) + " is over 40.</p>";
+      } else {
+        text = "<p>" + "Face " + (i + 1) + " is not over 40.</p>";
+      }
       results.innerHTML += text;
     }
-  });
 });
-
 btnDetermine.addEventListener("click", function () {
   results.innerHTML = " ";
-  console.log(faces);
   for (let i = 0; i < faces.length; i++) {
-    if (faces[i].age > 49) {
-      faces[i].finalComment = "<p>" + "Face " + (i + 1) + " is allowed in." + "</p>";
+    if (faces[i].age > 40) {
+      text = "<p>" + "Face " + (i + 1) + " is allowed in." + "</p>";
     } else {
-      faces[i].finalComment = '<p>' + "Face " + (i + 1) + " is not allowed in." + '<p>';
+      text = '<p>' + "Face " + (i + 1) + " is not allowed in." + '<p>';
     }
-    results.innerHTML += faces[i].finalComment;
+    results.innerHTML += text;
   }
 });
-
 btnClear.addEventListener("click", function () {
   let cleared = "<p>" + " " + "</p>";
   results.innerHTML = cleared;
